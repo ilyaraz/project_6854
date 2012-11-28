9f83278e24f2229b9b25c0c783308372c9f5908f
Ilya Razenshteyn <ilyaraz@gmail.com>
2012-11-28 09:48:29 -0500

    BSS

--- code/plot.r
+++ code/plot.r
@@ -1,7 +1,9 @@
 data_random = read.table("random.txt")
 data_grid = read.table("grid.txt")
+data_trees = read.table("trees.txt")
 
 pdf("plot.pdf")
-plot(data_grid, type = "l", col = "red")
+plot(data_trees, type = "l", col = "red")
 lines(data_random, col = "blue")
+lines(data_grid, col = "green")
 
